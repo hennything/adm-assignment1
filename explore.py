6430688f540cc954b0d8e16adddaa193d2f7d383
NNUGNHOJ <gunn.script@gmail.com>
2020-10-02 14:31:28 +0200

    explore.py changed to show headers

--- explore.py
+++ explore.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-ratings = pd.read_csv('ml-1m/ratings.dat', header=None, sep='::', engine='python')
-movies = pd.read_csv('ml-1m/movies.dat', header=None, sep='::', engine='python')
-users = pd.read_csv('ml-1m/users.dat', header=None, sep='::', engine='python')
+ratings = pd.read_csv('ml-1m/ratings.dat', header=None, sep='::', engine='python',
+                      names=["User ID", "Movie ID", "Rating", "Timestamp"])
+movies = pd.read_csv('ml-1m/movies.dat', header=None, sep='::', engine='python',
+                     names=["Movie ID", "Title", "Genres"])
+users = pd.read_csv('ml-1m/users.dat', header=None, sep='::', engine='python',
+                    names=["User ID", "Gender", "Age", "Occupation", "Zip-code"])
+
+print(ratings.head(3))
+print(movies.head(3))
+print(users.head(3))
