20b4e6418aed6319528423e2c6890eda7be9086c
John <gunn.script@gmail.com>
2020-10-06 10:04:35 +0200

    Matrix M is built

--- UV_matrix_decomp.py
+++ UV_matrix_decomp.py
@@ -7,13 +7,14 @@ ratings = pd.read_csv('ml-1m/ratings.dat', header=None, sep='::', engine='python
 user_IDs = ratings['User ID'].unique()
 movie_IDs = ratings['Movie ID'].unique()
 
+print('There are in total ' + str(len(movie_IDs)) + ' movies in the database')
+print('There are in total ' + str(len(user_IDs)) + ' users in the database')
+
 df = pd.DataFrame(index=user_IDs, columns=movie_IDs)
 
 for index, row in ratings.iterrows():
-    current_row = df.iloc[row['User ID']]
+    current_row = df.iloc[row['User ID'] - 1]
     current_row[row['Movie ID']] = row['Rating']
-
-    #print(row['User ID'], row['Movie ID'], row['Rating'])
 
 print(df.head(10))
 
