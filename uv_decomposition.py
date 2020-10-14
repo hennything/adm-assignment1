a799d9ec68e3d2de0b787e7c0fe84f0bcaca43af
hennything <hennything@MacBook-Pro.local>
2020-10-14 12:51:10 +0200

    incremental changes

--- uv_decomposition.py
+++ uv_decomposition.py
@@ -26,32 +26,45 @@ def initialize(train, d):
 
     return U, V
 
+# this needs to be looking at a single row of the UV matrix and compare it to a single row in the M matrix 
 def optimize_elem_u(U, V, u, i):
-    print(utility_matrix[u][i] - np.dot(U, V)[u][i])
-    # print(U[u][i] * V[i][] )
+    iss = M[u] - np.dot(U[u][i], V[i])
+
+    # np.where(~np.isnan())
+    # test
+    print(np.where(M[u] >= 1))
 
 # def optimize_elem_v(U, V, v, i):
 
 ##### this is currently where we are stuck
 def optimize(U, V, train, d):
+    # in here we still have to insert some sort of stop condition based on weather or not the difference is making significant steps
     for i in range(d):
         for u in range(U.shape[0]):
             optimize_elem_u(U, V, u, i)
+            # U[u][i] = 
             # call funciton to update element of U, def optimize_elem_u():
             # print(i, u)
         # for v in range(V.shape[1]):   
+            # V[v][i] =     or is it V[i][v]?
             # call funciton to update element of V, def optimize_elem_v():
             # print(i, v)
+
+
 #####
 ratings = pd.read_csv('ml-1m/ratings.dat', header=None, sep='::', engine='python', names=["User ID", "Movie ID", "Rating", "Timestamp"])
-utility_matrix = ratings.pivot(index='User ID', columns='Movie ID', values='Rating').to_numpy()
-m, n = utility_matrix.shape
-train = preprocess(ratings)
+M = ratings.pivot(index='User ID', columns='Movie ID', values='Rating').fillna(-1).to_numpy()
+m, n = M.shape
 
+# test training split isnt implemented in preprocess yet, once that is done then we can implement kfold cross validation
+train = preprocess(ratings)
+# test = preprocess(ratings, False)
+
+# this is the rank of the two uv matrices, I keep using the term rank but dont actually know if its the right term. 
 d = 5
 
 U, V = initialize(train, d)
 
 optimize(U, V, train, d)
 
-# kf = KFold(n_splits=5, shuffle=True, random_state=42)
+# P = np.dot(U, V)
