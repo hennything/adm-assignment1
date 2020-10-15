0271b687f19b7e5b36213301f8c173765fab63f2
hennything <hennything@MacBook-Pro.local>
2020-10-15 12:39:49 +0200

    part of example works

--- uv_decomposition.py
+++ uv_decomposition.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np 
 from math import sqrt
 from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+
 
 ##### will set this up to do a test training split
 def preprocess(table, train=True):
@@ -27,6 +29,7 @@ def initialize(train, d):
     return U, V
 
 def optimise_u(U, V, row_nr, M_row, i):
+    print("optimise U:")
     # print(
     #     "the row:", row, "\n",
     #     "\nrow number: ",row_nr, "\n",
@@ -34,21 +37,39 @@ def optimise_u(U, V, row_nr, M_row, i):
     #     
     V_temp = V.copy()
     V_cols = V_temp[:, np.where(~np.isnan(M_row))].reshape(2, -1)
+    # # print(V_cols.shape)
     iss = np.dot(U[row_nr, :], V_cols) - V_cols[i, :]
+    # # print(iss, "\n")
+    # # print(V_cols[i, :], "\n")
+    # # print((V_cols[i, :] * (M_row[np.where(~np.isnan(M_row))] - iss)).sum(), "\n")
+    # print(iss, V_cols, V_cols[i, :])
     return (V_cols[i, :] * (M_row[np.where(~np.isnan(M_row))] - iss)).sum() / (V_cols[i, :]**2).sum()
 
 def optimise_v(U, V, col_nr, M_col, i):
+    print("optimise V:")
     U_temp = U.copy()
     U_rows = U_temp[np.where(~np.isnan(M_col)), :].reshape(2, -1)
+    # # print(U_rows.shape)
     iss = np.dot(V[:, col_nr], U_rows) - U_rows[i, :]
-    return (U_rows * (M_col[np.where(~np.isnan(M_col))] - iss)).sum() / (U_rows[i, :]**2).sum()
+    # # print(iss, "\n")
+    # # print(U_rows[i, :], "\n")
+    # print(iss, U_rows, U_rows[i, :])
+    print((U_rows * (M_col[np.where(~np.isnan(M_col))] - iss)).sum(), "\n")
+    return (U_rows[i, :] * (M_col[np.where(~np.isnan(M_col))] - iss)).sum() / (U_rows[i, :]**2).sum()
 
 def rmse(U, V, M):
     # print(np.nansum(M))
-    print(np.where(~np.isnan(M))[0].sum())
-    print(np.dot(U, V)[np.where(~np.isnan(M))].sum())
+    print("RMSE: \n")
+    # print(M[np.where(~np.isnan(M))], "\n")
+    # print(np.dot(U, V)[np.where(~np.isnan(M))], "\n")
+    print(M[np.where(~np.isnan(M))], np.dot(U, V)[np.where(~np.isnan(M))], "\n")
+    print(np.dot(U, V)[np.where(~np.isnan(M))])
+    print("\n")
+    print(sqrt(mean_squared_error(M[np.where(~np.isnan(M))], np.dot(U, V)[np.where(~np.isnan(M))])), "RMSE")
+    # print(np.where(~np.isnan(M))[0].sum())
+    # print(np.dot(U, V)[np.where(~np.isnan(M))].sum())
     # print(np.where(~np.isnan(M))[0].sum() - np.dot(U, V)[np.where(~np.isnan(M))].sum())
-    print(sqrt(np.where(~np.isnan(M))[0].sum() / np.dot(U, V)[np.where(~np.isnan(M))].sum()))
+    # print(sqrt(np.where(~np.isnan(M))[0].sum() / np.dot(U, V)[np.where(~np.isnan(M))].sum()))
 
 
 #####
@@ -60,22 +81,23 @@ def fit(U, V, M, d):
             U[u, i] = optimise_u(U, V, u, M[u], i)
             # print(U[u, i])
             # if u == 5:
-                # break
+            break
         for v in range(V.shape[1]):
             # print(V[i, v])
             V[i, v] = optimise_v(U, V, v, M[:, v], i)
             # print(V[i, v])
             # if v == 5:
-                # break
+            break
+        break
     rmse(U, V, M)
     # print(np.dot(U, V))
 
 #####
 ratings = pd.read_csv('ml-1m/ratings.dat', header=None, sep='::', engine='python', names=["User ID", "Movie ID", "Rating", "Timestamp"])
-M = ratings.pivot(index='User ID', columns='Movie ID', values='Rating').to_numpy()
+# M = ratings.pivot(index='User ID', columns='Movie ID', values='Rating').to_numpy()
 
-print(type(M))
-# M = np.array([[5, 2, 4, 4, 3], [3, 1, 2, 4, 1], [2, np.nan, 3, 1, 4], [2, 5, 4, 3, 5], [4, 4, 5, 4, np.nan]]).astype(float)
+# print(type(M))
+M = np.array([[5, 2, 4, 4, 3], [3, 1, 2, 4, 1], [2, np.nan, 3, 1, 4], [2, 5, 4, 3, 5], [4, 4, 5, 4, np.nan]]).astype(float)
 
 # test training split isnt implemented in preprocess yet, once that is done then we can implement kfold cross validation
 train = preprocess(ratings)
@@ -83,16 +105,18 @@ train = preprocess(ratings)
 
 # this is the rank of the two uv matrices, I keep using the term rank but dont actually know if its the right term. 
 d = 2
-# U = np.full((5, 2), 1).astype(float)
-# V = np.full((2, 5) ,1).astype(float)
+U = np.full((5, 2), 1).astype(float)
+V = np.full((2, 5) ,1).astype(float)
 
-U, V = initialize(train, d)
+# U, V = initialize(train, d)
 # print(np.dot(U, V ))
 
-print(U.shape, V.shape)
+# print(U.shape, V.shape)
 rmse(U, V, M)
 fit(U, V, M, d)
-print("")
+# print(U, V)
+# print("")
+# print(np.dot(U, V))
 # fit(U, V, M, d)
 # print("")
 # fit(U, V, M, d)
